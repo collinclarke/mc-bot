62d58b9c52318e3903736d33d9603aa0ce545875
Collin Clarke <collinclarke@gmail.com>
2020-11-14 21:55:32 -0500

    Basic pathfinder bot

--- index.js
+++ index.js
@@ -1,18 +1,206 @@
 require('dotenv').config()
 
-const mineflayer = require('mineflayer')
 
-const bot = mineflayer.createBot({
+const mineflayer = require('mineflayer')
+const mineflayerViewer = require('prismarine-viewer').mineflayer
+const { pathfinder, Movements } = require('mineflayer-pathfinder')
+const { GoalBlock, GoalFollow, GoalInvert, GoalNear, GoalXZ, GoalY } = require('mineflayer-pathfinder').goals
+
+const allowedUsers = {}
+
+function authorize (username) {
+  allowedUsers[username] = {
+    listening: false
+  }
+}
+
+function deauthorize (username) {
+  allowedUsers[username] = false
+}
+
+function setListen (username, listen, bot) {
+  if (allowedUsers[username]) {
+    allowedUsers[username].listening = listen
+    return true
+  } else {
+    return false
+  }
+}
+
+function listening (username) {
+  return allowedUsers[username] && allowedUsers[username].listening
+}
+
+function generateMessage () {
+  const messageIndex = Math.floor(Math.random() * 10)
+  const messages = [
+    'leave me alone',
+    'stop',
+    'i do not take commands from you',
+    'shh',
+    'stfu',
+    'lol',
+    'nerd',
+    'ok quiet now',
+    'why should i?',
+    'who are you?',
+    'you are not my friend'
+  ]
+  return messages[messageIndex]
+}
+
+const options = {
   host: process.env.AZYROS_ADDRESS,
   username: process.env.BOT_USERNAME,
   password: process.env.BOT_PASSWORD
-})
+}
 
-bot.on('chat', function (username, message) {
-  if (username === bot.username) return
-  bot.chat(message)
-})
+function login() {
+  bot = mineflayer.createBot(options);
+  setBehavior(bot);
+}
 
-// Log errors and kick reasons:
-bot.on('kicked', (reason, loggedIn) => console.log(reason, loggedIn))
-bot.on('error', err => console.log(err))
+function setBehavior(bot) {
+  bot.loadPlugin(pathfinder)
+  bot.on('login', function() {
+    console.log("I logged in.");
+    console.log("settings", bot.settings);
+  });
+
+  bot.on('kicked', function(reason) {
+    console.log("I got kicked for", reason, "lol");
+    login()
+  });
+
+  bot.on('error', err => {
+    console.log(err)
+    bot.quit()
+    login()
+  })
+  
+  bot.once('spawn', () => {
+    authorize('lilcollin')
+    authorize('coolknife')
+    authorize('Banque')
+    authorize('Yungsuuu')
+    mineflayerViewer(bot, { port: 3007 })
+
+    // Once we've spawn, it is safe to access mcData because we know the version
+    const mcData = require('minecraft-data')(bot.version)
+
+    // We create different movement generators for different type of activity
+    const defaultMove = new Movements(bot, mcData)
+
+    bot.on('path_update', (r) => {
+      const nodesPerTick = (r.visitedNodes * 50 / r.time).toFixed(2)
+      console.log(`I can get there in ${r.path.length} moves. Computation took ${r.time.toFixed(2)} ms (${nodesPerTick} nodes/tick).`)
+    })
+
+    bot.on('goal_reached', (goal) => {
+      console.log('Here I am !')
+    })
+
+    const chatFunctionality = (whisper) => (username, message) => {
+      if (username === bot.username) return
+      if (username === 'you') return
+      const respond = (answer) => {
+        if (whisper) {
+          bot.whisper(username, answer)
+        } else {
+          bot.chat(answer)
+        }
+      }
+
+      if (message.toLowerCase().includes('listen')) {
+        const success = setListen(username, true, bot)
+        if (success) {
+          respond('ok sure whats up? try "come", "goto", "follow", "avoid", "stop"')
+        } else {
+          respond(generateMessage())
+        }
+      }
+
+      if (message === 'stop listening') {
+        const success = setListen(username, false)
+        if (success) {
+          respond('ok i will leave you alone')
+        }
+      }
+
+      if (!listening(username)) {
+        if (whisper) {
+          respond('I was told not to talk to strangers')
+        } else {
+          if (message.toLowerCase().includes('quadratum')) {
+            respond('Hello stranger, ask me to listen if you want me to follow commands')
+          }
+        }
+        return
+      }
+
+      const player = bot.players[username]
+      const target = player.entity
+
+      const isTargetNull = (bot) => {
+      if (!target || !target.position) {
+        bot.chat('I have no idea where you are')
+        return true
+      } else {
+        return false
+      }
+      }
+
+      if (!target) {
+        respond('')
+      }
+
+      if (message === 'come') {
+        if (isTargetNull(bot)) return
+        const p = target.position
+
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalNear(p.x, p.y, p.z, 1))
+      } else if (message.startsWith('goto')) {
+        const cmd = message.split(' ')
+
+        if (cmd.length === 4) { // goto x y z
+          const x = parseInt(cmd[1], 10)
+          const y = parseInt(cmd[2], 10)
+          const z = parseInt(cmd[3], 10)
+
+          bot.pathfinder.setMovements(defaultMove)
+          bot.pathfinder.setGoal(new GoalBlock(x, y, z))
+        } else if (cmd.length === 3) { // goto x z
+          const x = parseInt(cmd[1], 10)
+          const z = parseInt(cmd[2], 10)
+
+          bot.pathfinder.setMovements(defaultMove)
+          bot.pathfinder.setGoal(new GoalXZ(x, z))
+        } else if (cmd.length === 2) { // goto y
+          const y = parseInt(cmd[1], 10)
+
+          bot.pathfinder.setMovements(defaultMove)
+          bot.pathfinder.setGoal(new GoalY(y))
+        }
+      } else if (message === 'follow') {
+        if (isTargetNull(bot)) return
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalFollow(target, 3), true)
+        // follow is a dynamic goal: setGoal(goal, dynamic=true)
+        // when reached, the goal will stay active and will not
+        // emit an event
+      } else if (message === 'avoid') {
+        if (isTargetNull(bot)) return
+        bot.pathfinder.setMovements(defaultMove)
+        bot.pathfinder.setGoal(new GoalInvert(new GoalFollow(target, 5)), true)
+      } else if (message === 'stop') {
+        bot.pathfinder.setGoal(null)
+      }
+  }
+
+  bot.on('chat', chatFunctionality(false))
+  bot.on('whisper', chatFunctionality(true))
+})
+}
+
+login()
