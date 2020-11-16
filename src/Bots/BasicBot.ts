966e2f969fbe125982ac2238f5a74ee18fd83ff4
Mark Fingerhut <msf567@gmail.com>
2020-11-15 20:17:22 -0500

    sleep half done

--- src/Bots/BasicBot.ts
+++ src/Bots/BasicBot.ts
@@ -1,4 +1,5 @@
 import * as mineflayer from 'mineflayer'
+var blockFinderPlugin = require('mineflayer-blockfinder')(mineflayer);
 import { generateFeelingsMessage } from '../Utilities/conversation';
 
 export default class BasicBot {
@@ -18,7 +19,10 @@ export default class BasicBot {
       })
     }
 
-    initPlugins (): void {}
+    initPlugins (): void {
+      this.bot.loadPlugin(blockFinderPlugin);
+
+    }
 
     public respond (answer: string, username?: string) {
       setTimeout(() => {
@@ -52,6 +56,21 @@ export default class BasicBot {
     parseMessage(username:string, message: string, whisper?:boolean) {
       if (message.toLowerCase().includes('how are you')) {
         this.respond(generateFeelingsMessage(), whisper ? username : null)
+      }
+
+      if(message.toLowerCase() == "sleep here")
+      {
+       // let p = this.bot.players[username];
+      // let bed = this.bot.blockAt( p.entity.position, extraInfos=true)
+      // this.bot.sleep(bed,()=>{});
+
+      var b =  this.bot.findBlock({
+        point: this.bot.entity.position,
+        matching: 56,
+        maxDistance: 256,
+        count: 1,
+      });
+
       }
     }
 }
