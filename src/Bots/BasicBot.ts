4559a6abd9650c255ba37fbb22b02a41054d3551
Collin Clarke <collinclarke@gmail.com>
2020-11-15 19:12:51 -0500

    Fix missin user

--- src/Bots/BasicBot.ts
+++ src/Bots/BasicBot.ts
@@ -34,10 +34,14 @@ export default class BasicBot {
 
     private handleMessage () {
       this.bot.on('chat', (username: string, message: string) => {
+        if (username === this.bot.username) return
+        if (username === 'you') return
         this.parseMessage(username, message)
         this.setTarget(this.bot.players[username])
       })
       this.bot.on('whisper', (username: string, message: string) => {
+        if (username === this.bot.username) return
+        if (username === 'you') return
         this.parseMessage(username, message, true)
         this.setTarget(this.bot.players[username])
       })
